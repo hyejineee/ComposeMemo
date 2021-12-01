a1e1143ecbb530baf8139c2d8f59b6358783ed85
hyejineee <dev.hyejin04@gmail.com>
2021-12-01 17:41:48 +0900

    [FEAT] 테마 컬러 설정
    다크 모드, 라이트 모드를 지원하기 위해 각각 모드일때 사용하는 color테마를 설정하였습니다.

--- app/src/main/java/com/example/composememoapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/composememoapp/ui/theme/Theme.kt
@@ -7,15 +7,17 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = DeepPurpleA200Light,
+    primaryVariant = DeepPurpleA200Dark,
+    secondary = DeepPurpleA200,
+    surface = Gray900
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = DeepPurpleA200,
+    primaryVariant = DeepPurpleA200Light,
+    secondary = DeepPurpleA200Dark,
+    surface = Gray50
 
     /* Other default colors to override
     background = Color.White,
