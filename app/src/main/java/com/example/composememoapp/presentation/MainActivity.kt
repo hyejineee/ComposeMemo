056f8944c2c7a3efcc13d77d948bcd44e3d7fc2c
hyejineee <dev.hyejin04@gmail.com>
2021-12-02 18:11:37 +0900

    [CHORE] Mockito 의존성 추가
    AddMemo버튼의 onClick이 호출되는지 확인하기 위해 Mockio를 추가하여 테스트 했습니다.

--- app/src/main/java/com/example/composememoapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/composememoapp/presentation/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.composememoapp.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.ExperimentalAnimationApi
 import com.example.composememoapp.presentation.ui.MemoApp
 
+@ExperimentalAnimationApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
