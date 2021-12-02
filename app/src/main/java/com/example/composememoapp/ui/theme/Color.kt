b773d0df5901d8e887b77d768f47346f8f3de704
hyejineee <dev.hyejin04@gmail.com>
2021-12-02 14:19:15 +0900

    [CHORE] ktlint적용

--- app/src/main/java/com/example/composememoapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/composememoapp/ui/theme/Color.kt
@@ -9,6 +9,6 @@ val Teal200 = Color(0xFF03DAC5)
 
 val DeepPurpleA200 = Color(0xff7c4dff)
 val DeepPurpleA200Light = Color(0xffb47cff)
-val DeepPurpleA200Dark= Color(0xff3f1dcb)
+val DeepPurpleA200Dark = Color(0xff3f1dcb)
 val Gray50 = Color(0xffF4F4F4)
 val Gray900 = Color(0xff212121)
