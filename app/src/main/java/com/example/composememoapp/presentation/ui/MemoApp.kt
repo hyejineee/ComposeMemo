1e6861c5197d6564711ada5535c120eaa6ccab81
hyejineee <dev.hyejin04@gmail.com>
2021-12-14 19:57:35 +0900

    [STYLE] 빌드 경고 수정

--- app/src/main/java/com/example/composememoapp/presentation/ui/MemoApp.kt
+++ app/src/main/java/com/example/composememoapp/presentation/ui/MemoApp.kt
@@ -15,6 +15,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.contentDescription
@@ -24,6 +25,7 @@ import androidx.navigation.compose.rememberNavController
 import com.example.composememoapp.R
 import com.example.composememoapp.presentation.theme.ComposeMemoAppTheme
 
+@ExperimentalComposeUiApi
 @ExperimentalAnimationApi
 @Composable
 fun MemoApp() {
@@ -69,6 +71,7 @@ fun HomeAddMemoFAB(
     }
 }
 
+@ExperimentalComposeUiApi
 @ExperimentalAnimationApi
 @Preview
 @Composable
