de31eddad84fc0066cd54836dfe65d4a2c1595bb
hyejineee <dev.hyejin04@gmail.com>
2021-12-14 17:42:28 +0900

    [FIX] 구성 변경시 Saver에 객체가 저장되지 않는 문제 해결

--- app/src/main/java/com/example/composememoapp/data/TextBlock.kt
+++ app/src/main/java/com/example/composememoapp/data/TextBlock.kt
@@ -1,30 +1,38 @@
 package com.example.composememoapp.data
 
+import android.os.Parcelable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.sp
 import com.example.composememoapp.presentation.ui.component.TextInput
 import com.example.composememoapp.util.model.InputState
 import com.example.composememoapp.util.model.TextInputSate
+import java.lang.reflect.Modifier
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class TextBlock(
     override var seq: Int,
     override var content: String
-) : ContentBlock<String> {
+) : ContentBlock<String>, Parcelable {
 
     @Composable
-    override fun drawOnlyReadContent() {
-        Text(text = content)
+    override fun drawOnlyReadContent(modifier: androidx.compose.ui.Modifier) {
+        Text(text = content, fontSize = 13.sp)
     }
 
     @Composable
-    override fun drawEditableContent(state: InputState) {
+    override fun drawEditableContent(state: InputState, modifier: androidx.compose.ui.Modifier) {
         val textInputState = (state as TextInputSate)
 
-        TextInput(
-            text = textInputState.text,
-            onValueChange = {
-                textInputState.text = it
-            }
-        )
+        Box(modifier = modifier) {
+            TextInput(
+                text = textInputState.text,
+                onValueChange = {
+                    textInputState.text = it
+                }
+            )
+        }
     }
 }
