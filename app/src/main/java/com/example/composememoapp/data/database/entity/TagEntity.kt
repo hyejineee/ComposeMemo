4bbf9ee6c22bc376170e0c386d7a8b3dc61f507e
hyejineee <dev.hyejin04@gmail.com>
2021-12-18 17:16:56 +0900

    [FIX] tag디비에 중복 태그가 들어가는 문제 해결

--- app/src/main/java/com/example/composememoapp/data/database/entity/TagEntity.kt
+++ app/src/main/java/com/example/composememoapp/data/database/entity/TagEntity.kt
@@ -1,9 +1,10 @@
 package com.example.composememoapp.data.database.entity
 
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(indices = arrayOf(Index(value = ["tag"], unique = true)))
 data class TagEntity(
     @PrimaryKey(autoGenerate = true) val id:Long? = null,
     val tag:String? = null
