d1642f0663d8903ac0efad08ee2dc08cc53e6498
hyejineee <dev.hyejin04@gmail.com>
2021-12-20 10:54:00 +0900

    [FEAT] TagViewModel 생성
    Tag와 관련된 비즈니스 로직을 수행하는 TagViewModel을 생성했습니다.
    유스케이스를 사용하여 모든 태그를 가져오는 getAllTag()를 구현했습니다.

--- app/src/test/java/com/example/composememoapp/viewModel/TagViewModelTest.kt
+++ app/src/test/java/com/example/composememoapp/viewModel/TagViewModelTest.kt
@@ -1,23 +1,41 @@
 package com.example.composememoapp.viewModel
 
+import com.example.composememoapp.data.database.entity.TagEntity
+import com.example.composememoapp.data.repository.MemoAppRepository
+import com.example.composememoapp.domain.GetAllTagUseCase
 import com.example.composememoapp.presentation.viewModel.TagViewModel
+import io.reactivex.rxjava3.kotlin.toFlowable
+import io.reactivex.rxjava3.observers.TestObserver
 import io.reactivex.rxjava3.schedulers.Schedulers
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.mockito.Mockito
+import org.mockito.kotlin.given
 
 class TagViewModelTest {
 
-    // 메모를 카테고리에 따라 필터링한다.
-    // 메모를 저장한다, 수정한다.
-    // 메모를 삭제한다.
+    private val testMemoRepository = Mockito.mock(MemoAppRepository::class.java)
 
     private val schedulers = Schedulers.newThread()
-    private val tagViewModel = TagViewModel()
+    private val getAllTagUseCase = GetAllTagUseCase(testMemoRepository)
+    private val tagViewModel = TagViewModel(
+        ioScheduler = schedulers,
+        androidScheduler = schedulers,
+        getAllTagUseCase = getAllTagUseCase
+    )
+
+    private val tagListMock = List(5){
+        TagEntity(tag = "tag$it")
+    }
 
     @Test
     @DisplayName("모든 태그를 가져온다.")
     fun getAllTagTest(){
+        given(testMemoRepository.getAllTag()).willReturn(listOf(tagListMock).toFlowable())
 
+        tagViewModel.getAllTag()
+
+        tagViewModel.tagList.test().assertValue(tagListMock)
     }
 
 }
