c61eff93bf7c4dce94c9ed1b1481bcf3a7c9563d
hyejineee <dev.hyejin04@gmail.com>
2021-12-24 15:09:56 +0900

    [DESIGN] 이미지가 있을 경우 메모 리스트 아이템 형태 변경
    메모에 이미지가 포함되어 있을 경우 이미지를 첫번째 이미지를 메모 아이템의 배경으로
    보여주도록 변경했습니다.

--- app/src/main/java/com/example/composememoapp/data/ImageBlock.kt
+++ app/src/main/java/com/example/composememoapp/data/ImageBlock.kt
@@ -2,27 +2,34 @@ package com.example.composememoapp.data
 
 import android.content.Context
 import android.graphics.Bitmap
-import android.graphics.ImageDecoder
+import android.media.Image
 import android.net.Uri
-import android.os.Build
 import android.os.Parcelable
-import android.provider.MediaStore
-import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import com.example.composememoapp.data.database.entity.ContentBlockEntity
-import java.lang.Exception
+import com.example.composememoapp.util.ImageProvider
+import dagger.hilt.EntryPoint
+import dagger.hilt.InstallIn
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.EntryPointAccessors
+import dagger.hilt.android.components.ViewComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import kotlinx.parcelize.IgnoredOnParcel
+import javax.inject.Inject
 
 @kotlinx.parcelize.Parcelize
 data class ImageBlock(
@@ -30,20 +37,30 @@ data class ImageBlock(
     override var content: Uri?,
 ) : ContentBlock<Uri?>, Parcelable {
 
-    lateinit var imageState: MutableState<Bitmap?>
+    @EntryPoint
+    @InstallIn(SingletonComponent::class)
+    interface ImageProviderEntryPoint {
+        fun imageProvider(): ImageProvider
+    }
+
+    @IgnoredOnParcel
+    var imageState: MutableState<Bitmap?> = mutableStateOf(null)
+
 
     @Composable
     override fun drawOnlyReadContent(modifier: Modifier) {
-        val context = LocalContext.current
-        imageState = remember { mutableStateOf<Bitmap?>(null) }
-        setImageState(context = context)
-
+        getBitmap(LocalContext.current)
         Box(modifier = modifier) {
             imageState.value?.let { btm ->
                 Image(
                     bitmap = btm.asImageBitmap(),
                     contentDescription = null,
-                    modifier = Modifier.align(Alignment.Center)
+                    modifier = Modifier.align(Alignment.Center),
+                    contentScale = ContentScale.FillBounds,
+                    colorFilter = ColorFilter.tint(
+                        Color(0x70000000),
+                        BlendMode.SrcOver
+                    )
                 )
             }
         }
@@ -51,17 +68,13 @@ data class ImageBlock(
 
     @Composable
     override fun drawEditableContent(modifier: Modifier) {
-
-        val context = LocalContext.current
-
-        setImageState(context = context)
-
+        getBitmap(LocalContext.current)
         Box(modifier = Modifier.fillMaxWidth()) {
             imageState.value?.let { btm ->
                 Image(
                     bitmap = btm.asImageBitmap(),
                     contentDescription = null,
-                    modifier = Modifier.align(Alignment.Center)
+                    modifier = Modifier.align(Alignment.Center),
                 )
             }
         }
@@ -73,18 +86,19 @@ data class ImageBlock(
         content = content.toString()
     )
 
-    private fun setImageState(context: Context) {
-        content?.let {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-                try {
-                    val source = ImageDecoder
-                        .createSource(context.contentResolver, it)
-                    imageState.value = ImageDecoder.decodeBitmap(source)
-                } catch (e: Exception) {
-                    Log.d("ImageBlock", "exception : ${e.message}")
-                }
-            } else {
-                imageState.value = MediaStore.Images.Media.getBitmap(context.contentResolver, it)
+    private fun getBitmap(context: Context) {
+
+        val imageProvider = EntryPointAccessors
+            .fromApplication(context, ImageProviderEntryPoint::class.java)
+            .imageProvider()
+
+        if (imageState.value == null && imageProvider != null) {
+            content?.let { uri ->
+                imageProvider.getBitmapFromFile(uri)
+                    .subscribe(
+                        { imageState.value = it },
+                        { imageState.value = null }
+                    )
             }
         }
     }
