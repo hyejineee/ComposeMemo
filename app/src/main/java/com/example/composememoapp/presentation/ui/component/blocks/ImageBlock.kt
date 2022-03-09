7f966e431a6cb84e21f5a50e10242fce8770b358
hyejineee <dev.hyejin04@gmail.com>
2022-03-09 16:59:49 +0900

    [Refactor] Glide 이미지 라이브러리 사용

--- app/src/main/java/com/example/composememoapp/presentation/ui/component/blocks/ImageBlock.kt
+++ app/src/main/java/com/example/composememoapp/presentation/ui/component/blocks/ImageBlock.kt
@@ -1,26 +1,26 @@
 package com.example.composememoapp.presentation.ui.component.blocks
 
-import android.content.Context
 import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Parcelable
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import com.example.composememoapp.data.database.entity.ContentBlockEntity
-import com.example.composememoapp.util.BitmapProvider
+import com.skydoves.landscapist.ShimmerParams
+import com.skydoves.landscapist.glide.GlideImage
 import kotlinx.parcelize.IgnoredOnParcel
 
 @kotlinx.parcelize.Parcelize
@@ -34,36 +34,44 @@ data class ImageBlock(
 
     @Composable
     override fun drawOnlyReadContent(modifier: Modifier) {
-        getBitmap(LocalContext.current)
-        Box(modifier = modifier) {
-            imageState.value?.let { btm ->
-                Image(
-                    bitmap = btm.asImageBitmap(),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .align(Alignment.TopCenter)
-                        .aspectRatio(1f),
-                    contentScale = ContentScale.Crop,
-                    colorFilter = ColorFilter.tint(
-                        Color(0x50000000),
-                        BlendMode.SrcOver
-                    )
-                )
-            }
+
+        Box(modifier = modifier.padding(16.dp)) {
+            GlideImage(
+                imageModel = content,
+                modifier = Modifier
+                    .align(Alignment.TopCenter)
+                    .aspectRatio(0.8f)
+                    .clip(RoundedCornerShape(16.dp)),
+                contentScale = ContentScale.Crop,
+                shimmerParams = ShimmerParams(
+                    baseColor = MaterialTheme.colors.background,
+                    highlightColor = Color.Gray,
+                    durationMillis = 500,
+                    dropOff = 0.65f,
+                    tilt = 20f
+                ),
+            )
         }
     }
 
     @Composable
     override fun drawEditableContent(modifier: Modifier) {
-        getBitmap(LocalContext.current)
+
         Box(modifier = modifier.fillMaxWidth()) {
-            imageState.value?.let { btm ->
-                Image(
-                    bitmap = btm.asImageBitmap(),
-                    contentDescription = null,
-                    modifier = Modifier.align(Alignment.Center),
-                )
-            }
+            GlideImage(
+                imageModel = content,
+                modifier = Modifier
+                    .align(Alignment.TopCenter)
+                    .clip(RoundedCornerShape(16.dp)),
+                contentScale = ContentScale.Crop,
+                shimmerParams = ShimmerParams(
+                    baseColor = MaterialTheme.colors.background,
+                    highlightColor = Color.Gray,
+                    durationMillis = 500,
+                    dropOff = 0.65f,
+                    tilt = 20f
+                ),
+            )
         }
     }
 
@@ -73,16 +81,4 @@ data class ImageBlock(
         content = content.toString()
     )
 
-    private fun getBitmap(context: Context) {
-        if (imageState.value == null) {
-            val bitmapProvider = BitmapProvider(context = context)
-            content?.let { uri ->
-                bitmapProvider.getBitmapFromFile(uri)
-                    .subscribe(
-                        { imageState.value = it },
-                        { imageState.value = null }
-                    )
-            }
-        }
-    }
 }
