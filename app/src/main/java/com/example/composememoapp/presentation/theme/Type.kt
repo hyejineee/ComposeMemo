3eba169117351390bf8deda76471b1340ce71d98
hyejineee <dev.hyejin04@gmail.com>
2022-03-09 18:28:42 +0900

    [Design] 폰트 파일 추가 및 변경

--- app/src/main/java/com/example/composememoapp/presentation/theme/Type.kt
+++ app/src/main/java/com/example/composememoapp/presentation/theme/Type.kt
@@ -2,27 +2,40 @@ package com.example.composememoapp.presentation.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.composememoapp.R
 
 // Set of Material typography styles to start with
-val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
-    ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
+//val Typography = Typography(
+//    body1 = TextStyle(
+//        fontFamily = FontFamily.Default,
+//        fontWeight = FontWeight.Normal,
+//        fontSize = 16.sp
+//    )
+/* Other default text styles to override
+button = TextStyle(
+    fontFamily = FontFamily.Default,
+    fontWeight = FontWeight.W500,
+    fontSize = 14.sp
+),
+caption = TextStyle(
+    fontFamily = FontFamily.Default,
+    fontWeight = FontWeight.Normal,
+    fontSize = 12.sp
+)
+*/
+//)
+
+
+private val regular = Font(R.font.s_core_dream_regular, FontWeight.Normal)
+private val medium = Font(R.font.s_core_dream_medium, FontWeight.Medium)
+private val extraBold = Font(R.font.reco_extra_bold, FontWeight.ExtraBold)
+
+private val appFontFamily = FontFamily(regular, medium, extraBold)
+
+val appFont = Typography(
+    defaultFontFamily = appFontFamily,
 )
